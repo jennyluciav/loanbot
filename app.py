d77e8cb2357d07baa1e308025709ef4748495f23
Jenny Vega <jvega1@trueblue.com>
2022-08-22 10:30:20 +0100

    added fixes to loanbot

--- app.py
+++ app.py
@@ -4,18 +4,21 @@ from loanbot import ask, append_interaction_to_chat_log
 
 app = Flask(__name__)
 # if for some reason your conversation with the chef gets weird, change the secret key 
-app.config['SECRET_KEY'] = 'top-secret!'
+app.config['SECRET_KEY'] = 'top-secret!1111'
 
 @app.route('/loanbot', methods=['POST'])
 def loan():
     incoming_msg = request.values['Body']
+    print("incoming_msg = ", incoming_msg)
     chat_log = session.get('chat_log')
     answer = ask(incoming_msg, chat_log)
+    print("answer = ", answer)
     session['chat_log'] = append_interaction_to_chat_log(incoming_msg, answer,
                                                          chat_log)
-    print("the session chat_log = ", chat_log)
+    #print("the session chat_log = ", chat_log)
     msg = MessagingResponse()
     msg.message(answer)
+    print("msg = ", msg)
     return str(msg)
 
 
